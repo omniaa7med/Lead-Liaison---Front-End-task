3fad3ca71b82ff6fd8674d22410f00794af30a26
omniaa <omniaahmed71098@gmail.com>
2022-06-01 01:14:04 +0200

    'Add firebase realtime-Database and Firestore Database'

--- Lead-Liaison-Front-End-task/src/app/app.module.ts
+++ Lead-Liaison-Front-End-task/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Lead-Liaison-Front-End-task/src/environments/environment.prod.ts
+++ Lead-Liaison-Front-End-task/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'lead-liaison-front-end-task',
+    appId: '1:363322269804:web:430efcaa9019aed11c7849',
+    databaseURL: 'https://lead-liaison-front-end-task-default-rtdb.firebaseio.com',
+    storageBucket: 'lead-liaison-front-end-task.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD-1ZpGPEi3Ad7KHT8LxTt9pWxkOT32scg',
+    authDomain: 'lead-liaison-front-end-task.firebaseapp.com',
+    messagingSenderId: '363322269804',
+  },
   production: true
 };
