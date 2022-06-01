2e705f5a74d8573af2b7f4380b0e90ffea2969e0
omniaa <omniaahmed71098@gmail.com>
2022-06-01 16:55:56 +0200

    'handel productList ui'

--- Lead-Liaison-Front-End-task/src/app/product-list/product-list.component.ts
+++ Lead-Liaison-Front-End-task/src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product';
+import { ProductListService } from '../service/product-list.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private productService: ProductListService) { }
+  products: Product[] = [];
   ngOnInit(): void {
+    this.productService.getProductList().subscribe((data: any) => {
+      // console.log(Object.keys(data).map((key) => data[key]));
+      this.products = Object.keys(data).map((key) => data[key])
+      console.log(this.products);
+    });
+
+
+
+    // this.productService.getUserList().subscribe((res) => {
+    //   console.log(res.map((e) => {
+    //     return {
+    //       // iD: e.payload.doc.id,
+    //       ...(e.payload.doc.data() as Product),
+    //     };
+    //   }));
+    // });
   }
 
 }
